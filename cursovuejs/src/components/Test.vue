6d0e05ce0aed0bd226af8f0f95e81c2f7b3aaa25
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-01-29 04:47:28 -0600

    Proyecto de vue creado y testeo de componentes

---
+++ cursovuejs/src/components/Test.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="text" v-on:keyup.enter="typing">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                titulo : 'Datos del usuario',
+                user : {
+                    nombre : 'Patricia',
+                    email : 'may.patrics@gmail.com',
+                },
+                showName : true,
+                perros : [
+                    {nombre : 'loveless' , edad : '3'},
+                    {nombre: 'dolly' , edad : '13'},
+                ]
+            }
+        },
+        methods:{
+            saludar (nombre) {
+                alert('Holis! ' + nombre);
+            },
+            typing (e) {
+                console.log(e.target.value);
+            },
+        }
+
+    }
+</script>
+
+<style media="screen">
+
+</style>
