223323618d5c685aca39e341fd56b432a1f1c14d
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-01-30 03:37:27 -0600

    Imprimir datos del componente y agregar nuevos datos a los prestablecidos con el metodo push

--- cursovuejs/src/App.vue
+++ cursovuejs/src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <h1>Hola</h1>
     <Test mensaje='mensaje enviado desde el componente padre'/>
+    <User/>
   </div>
 </template>
 
 <script>
 import Test from './components/Test.vue'
+import User from './components/User.vue'
 export default {
   name: 'App',
   components: {
-    Test
+    Test,
+    User
   }
 }
 </script>
---
+++ cursovuejs/src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="users">
+        <h1>Componente de users</h1>
+        <ul>
+            <li v-for="user in users">
+                {{user.nombre}} - {{user.apellido}}
+            </li>
+        </ul>
+        <form v-on:submit.prevent='addUser'>
+            <input type="text" v-model="newUser.nombre" placeholder="nombre">
+            <input type="text" v-model="newUser.apellido" placeholder="apellido">
+            <input type="email" v-model="newUser.email" placeholder="email">
+            <button type="submit">Añadir</button>
+        </form>
+    </div>
+</template>
+<script>
+
+export default {
+    data(){
+        return{
+            users:[
+                {
+                    nombre:'Patricia',
+                    apellido:'Perez',
+                    email:'pato@gmail.com',
+                    contacto:false,
+                },
+                {
+                    nombre:'Daniela',
+                    apellido:'Rubiales',
+                    email:'dani@gmail.com',
+                    contacto:true,
+                },
+                {
+                    nombre:'Gabriela',
+                    apellido:'Curiel',
+                    email:'gabi@gmail.com',
+                    contacto:true,
+                }
+            ],
+            newUser:{}
+        }
+    },
+    methods: {
+        addUser(){
+            this.users.push(this.newUser);
+            this.newUser = {};
+        },
+    },
+}
+</script>
+<style>
+    .users{
+        background: lightcyan;
+    }
+</style>
